2fa5c6e91c3703c7f8f0724881c5d4e6333738a4
Maximiliano Levi <maxilevi77@gmail.com>
2021-07-01 17:59:33 -0300

    add basic team model

--- app/models/team.go
+++ app/models/team.go
@@ -1,1 +1,20 @@
 package models
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Team struct {
+	gorm.Model
+	Name string
+	Country string
+	Players []Player
+}
+
+func (team *Team) MarketValue() int32 {
+	var sum int32
+	for _, player := range team.Players {
+		sum += player.MarketValue
+	}
+	return sum
+}
