afe58a4bc971371d59d89cc943b036cbbd029e6e
Maximiliano Levi <maxilevi77@gmail.com>
2021-07-02 00:24:13 -0300

    add user registration code

--- app/handlers/user.go
+++ app/handlers/user.go
@@ -1,15 +1,70 @@
 package handlers
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
+	"log"
 	"net/http"
+	"net/mail"
+	"../models"
 )
 
 func AddUserRoutes(r *mux.Router) {
 	r.HandleFunc("/users", handlePostUser).Methods( "POST")
 }
 
-func handlePostUser(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hi!!!")
+type userRegistration struct {
+	Email string
+	Password string
+}
+
+func handlePostUser(w http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var t userRegistration
+	err := decoder.Decode(&t)
+	if err != nil {
+		writeError(w, http.StatusBadRequest, "Incorrect body parameters")
+		return
+	}
+	if !validEmail(t.Email) {
+		writeError(w, http.StatusBadRequest, "Invalid email")
+		return
+	}
+	if !validPassword(t.Password) {
+		writeError(w, http.StatusBadRequest, "Password needs a minimum of at least 8 characters")
+		return
+	}
+
+	log.Println("Registering new user...")
+
+	err = registerUser(t)
+	if err != nil {
+		writeError(w, http.StatusInternalServerError, "Internal server error")
+		return
+	}
+
+	writeResponse(w, 200, []byte(`{"error": false}`))
+}
+
+func validPassword(password string) bool {
+	return len(password) >= 8
+}
+
+func validEmail(email string) bool {
+	_, err := mail.ParseAddress(email)
+	return err == nil
+}
+
+func registerUser(reg userRegistration) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(reg.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	user := models.User{}
+	user.Email = reg.Email
+	user.PasswordHash = hashedPassword
+	user.PermissionLevel = 0
+	return nil
 }
--- app/handlers/util.go
+++ app/handlers/util.go
@@ -1,1 +1,22 @@
 package handlers
+
+import (
+	"log"
+	"net/http"
+)
+
+func writeResponse(w http.ResponseWriter, code int, message [] byte) {
+	w.WriteHeader(code)
+	_, err := w.Write(message)
+	if err != nil {
+		log.Printf("Failed to write response")
+	}
+}
+
+func writeError(w http.ResponseWriter, code int, message string) {
+	w.WriteHeader(code)
+	_, err := w.Write([]byte(`{"error": true, "message": "` + message + `"}`))
+	if err != nil {
+		log.Printf("Failed to write response")
+	}
+}
