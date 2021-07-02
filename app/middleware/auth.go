b7087ed61e697e4539637ee117e4c172b72e7a75
Maximiliano Levi <maxilevi77@gmail.com>
2021-07-02 20:32:26 -0300

    make jwt secret an env key

--- app/middleware/auth.go
+++ app/middleware/auth.go
@@ -1,15 +1,18 @@
 package middleware
 
 import (
+	"../models"
+	"../repos"
 	"fmt"
 	"github.com/golang-jwt/jwt"
 	"github.com/gorilla/context"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
-var JWTKey = []byte("fantasy_football")
+var JWTKey = []byte(os.Getenv("JWT_SECRET"))
 
 type AuthClaims struct {
 	Email string `json:"email"`
@@ -24,38 +27,49 @@ func failedAuth(w http.ResponseWriter, code int, message string) {
 	}
 }
 
-func Auth(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		authHeader := r.Header.Get("Authorization")
-		splitToken := strings.Split(authHeader, "Bearer ")
-		if len(splitToken) != 2 {
-			failedAuth(w, http.StatusBadRequest, `{error: true, message: "Bad request"}`)
-			return
-		}
-		tokenString := splitToken[1]
-		claims := &AuthClaims{}
-
-		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-			}
-			return JWTKey, nil
-		})
-		if err != nil {
-			if err == jwt.ErrSignatureInvalid {
-				failedAuth(w, http.StatusUnauthorized, `{error: true, message: "Unauthorized"}`)
+func Auth(repo repos.Repository) func (next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			authHeader := r.Header.Get("Authorization")
+			splitToken := strings.Split(authHeader, "Bearer ")
+			if len(splitToken) != 2 {
+				failedAuth(w, http.StatusBadRequest, "Bad request")
 				return
 			}
-			failedAuth(w, http.StatusBadRequest, `{error: true, message: "Bad request"}`)
-			return
-		}
-		if token.Valid {
-			context.Set(r, "token", token)
-			context.Set(r, "email", claims.Email)
-			next.ServeHTTP(w, r)
-		} else {
-			failedAuth(w, http.StatusUnauthorized, `{error: true, message: "Unauthorized"}`)
-		}
-	})
+			tokenString := splitToken[1]
+			claims := &AuthClaims{}
+
+			token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+				}
+				return JWTKey, nil
+			})
+
+			if err != nil {
+				if err == jwt.ErrSignatureInvalid {
+					failedAuth(w, http.StatusUnauthorized, "Unauthorized")
+					return
+				}
+				failedAuth(w, http.StatusBadRequest, "Bad request")
+				return
+			}
+
+			if token.Valid {
+				context.Set(r, "token", token)
+				context.Set(r, "email", claims.Email)
+				var user models.User
+				err = repo.GetUser(claims.Email, &user)
+				if err != nil {
+					failedAuth(w, http.StatusUnauthorized, "Unauthorized")
+					return
+				}
+				context.Set(r, "user", user)
+				next.ServeHTTP(w, r)
+			} else {
+				failedAuth(w, http.StatusUnauthorized, "Unauthorized")
+			}
+		})
+	}
 }
 
