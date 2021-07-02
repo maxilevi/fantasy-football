a56ffea958c12457948f2da85dc269dd82a9d6fc
Maximiliano Levi <maxilevi77@gmail.com>
2021-07-02 13:06:21 -0300

    add a test for tests

--- app/models/team_test.go
+++ app/models/team_test.go
@@ -1,1 +1,20 @@
 package models
+
+import "testing"
+
+func TestTeamValue(t *testing.T) {
+	players := make([]Player, 20)
+	for i := 0; i < len(players); i++ {
+		players[i] = RandomPlayer()
+	}
+	team := Team{
+		Players: players,
+	}
+	var sum int32
+	for i := range players {
+		sum += players[i].MarketValue
+	}
+	if sum != team.MarketValue() {
+		t.Error("Team value is not summed player value")
+	}
+}
